1236c5cc8bef9607b8b48321651d99c8058ecec4
srazihaider <34131796+srazihaider@users.noreply.github.com>
2018-11-11 14:16:19 +0100

    Update Binarygap.cpp

--- Binarygap.cpp
+++ Binarygap.cpp
@@ -1,1 +1,71 @@
 
+// you can use includes, for example:
+// #include <algorithm>
+
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+
+int solution(int N) {
+    // write your code in C++14 (g++ 6.2.0)
+    
+    int remainder = 0;
+    bool prev = false;
+    bool firstremainder= false;
+    int binarygap=0;
+    
+    while (N=!0)
+    
+    {
+        
+        Remainder = N%2;
+        
+        if(Remainder == 0 && prev ==false && firstremainder==true )
+        
+        {
+            
+            
+            
+            
+        }
+        
+       else if (Remainder == 1 && prev == true && firstremainder == true)
+        
+        {
+            
+            
+            
+            
+            
+        }
+        
+        
+       else if (Remainder == 1 && prev == false)
+        
+        
+        {
+            
+            
+            
+            
+        }
+    
+    
+        else
+        
+        {
+            
+            
+            
+            
+            
+            
+        }
+    
+
+        
+      N=N/2;
+        
+    }
+    
+    return binarygap;  
+}
