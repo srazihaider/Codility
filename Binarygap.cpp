1301be315cb3d7c9230d9376dee488d60386b2e5
srazihaider <34131796+srazihaider@users.noreply.github.com>
2018-11-11 18:37:38 +0100

    Update Binarygap.cpp

--- Binarygap.cpp
+++ Binarygap.cpp
@@ -1,4 +1,3 @@
-
 // you can use includes, for example:
 // #include <algorithm>
 
@@ -8,35 +7,51 @@
 int solution(int N) {
     // write your code in C++14 (g++ 6.2.0)
     
-    int remainder = 0;
-    bool prev = false;
-    bool firstremainder= false;
+    int Remainder = 0;
     int binarygap=0;
-    
-    while (N=!0)
+    bool prev = false;
+    int counter=0;
+    bool isfirstremainder=false;
+    while (N!=0)
     
     {
-        
         Remainder = N%2;
-        
-        if(Remainder == 0 && prev ==false && firstremainder==true )
+        if(Remainder == 0 && prev == true && isfirstremainder==true )
         
         {
+          counter++;
+          prev=true;
             
-            
+        }
+        
+       else if (Remainder == 0 && prev == false && isfirstremainder ==true )
+        
+        {
+            counter =1;
+            prev= true;
             
             
         }
         
-       else if (Remainder == 1 && prev == true && firstremainder == true)
+        else if (Remainder==1)
         
-        {
+         {
+           
+           if(binarygap<counter)
+           binarygap=counter;
             
+            prev=false;
             
+            isfirstremainder=true;
             
-            
-            
-        }
+         }
+         
+        N=N/2; 
+         
+    }
+
+     return binarygap;   
+}
         
         
        else if (Remainder == 1 && prev == false)
