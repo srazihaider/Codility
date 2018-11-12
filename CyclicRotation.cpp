62abe19bc27def137cdb5bb8b78b53ad2a84085b
srazihaider <34131796+srazihaider@users.noreply.github.com>
2018-11-12 12:36:53 +0100

    Create CyclicRotation.cpp

---
+++ CyclicRotation.cpp
@@ -0,0 +1,35 @@
+// you can use includes, for example:
+
+//#include <algorithm>
+
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+
+vector<int> solution(vector<int> &A, int K) {
+    // write your code in C++14 (g++ 6.2.0)
+    
+ vector<int> returnvector;
+ int counter=0;
+int dividing_line = A.size() - K;
+
+for(int i = dividing_line; i<A.size();i++ )
+
+{
+    returnvector.push_back(A[i]) ;
+     counter++;
+}
+
+
+
+for (int i = 0; i<dividing_line; i++)
+
+{
+
+    returnvector.push_back(A[i]);
+
+}
+    
+return returnvector;
+
+    
+}
