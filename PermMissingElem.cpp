c960b575d48a68330804ef8b3680f62c203b226d
srazihaider <34131796+srazihaider@users.noreply.github.com>
2018-11-13 13:45:00 +0100

    Update PermMissingElem.cpp

--- PermMissingElem.cpp
+++ PermMissingElem.cpp
@@ -3,12 +3,8 @@
 // The logic here is to calculate the expected sum ( Gauss's sum) and also calculate the given sum
 // The difference between the two will be the missing number 
 
-
 int solution(vector<int> &A) {
  
-    // write your code in C++14 (g++ 6.2.0)
-    
-    
     int sum=0;
     
  for (unsigned int i=0 ; i<A.size();i++)
@@ -19,7 +15,4 @@ int solution(vector<int> &A) {
  int gauss_sum= (A.size()+2)*(A.size()+1)/2;
  
  return gauss_sum-sum;
- 
- 
- 
 }
