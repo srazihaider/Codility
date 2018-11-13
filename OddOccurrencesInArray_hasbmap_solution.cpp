77401da1f69db67cffbacdf75f1c7fb62e8a9c8f
srazihaider <34131796+srazihaider@users.noreply.github.com>
2018-11-13 13:43:57 +0100

    Update OddOccurrencesInArray_hasbmap_solution.cpp

--- OddOccurrencesInArray_hasbmap_solution.cpp
+++ OddOccurrencesInArray_hasbmap_solution.cpp
@@ -1,11 +1,4 @@
-// you can use includes, for example:
-// #include <algorithm>
-
 #include <unordered_map>
-
-// you can write to stdout for debugging purposes, e.g.
-// cout << "this is a debug message" << endl;
-
 int solution(vector<int> &A) {
     // write your code in C++14 (g++ 6.2.0)
     
